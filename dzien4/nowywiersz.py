5640183e804b3401c467beded89329ea8f26afa6
Lewixx <39681237+Lewixx@users.noreply.github.com>
2018-06-06 20:29:33 +0200

    pierwsze pliki poprawione

--- dzien4/nowywiersz.py
+++ dzien4/nowywiersz.py
@@ -1,3 +1,7 @@
 with open("moj_wiersz.txt", "w") as plik:
     print("ok")
-    
+    # r - tylko do odczytu
+    # w otwiera usuwa zawartosc i do zapisu
+    # a otweira i kursor na koncu do zapisu
+    # r+ do oczytu i zapisu
+    # czyta linie, calosc, funkcja open
