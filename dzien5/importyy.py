6ec392940147c2cfafe6fb9ca8feddc733eddac8
Lewixx <39681237+Lewixx@users.noreply.github.com>
2018-06-06 20:29:09 +0200

    pierwsze pliki poprawione

---
+++ dzien5/importyy.py
@@ -0,0 +1,11 @@
+import antigravity
+antigravity
+
+import random
+print(random.randint(23, 213123))
+print(random.random())
+print(random.choice(["ala", "marek", "ziom"]))
+
+import os
+print(os.getcwd())
+#library reference
