2498648b4827868e2f43b765f44c08a5e056acc5
Lewixx <39681237+Lewixx@users.noreply.github.com>
2018-06-11 20:34:50 +0200

    pierwsze pliki poprawione

---
+++ dzien7/modulos.py
@@ -0,0 +1,10 @@
+import os
+for folder, podfolder, pliki in os.walk('C:\\Users\\Ja\\PycharmProjects\\lewypython')
+
+    for p in podfolder:
+        print(f'{folder} / {podfolder}')
+        #oddaje nam tuple
+        os.path
+
+        sciezka = os.path.join('sdfsdf','plik.txt')
+        print(os.getcwd())
